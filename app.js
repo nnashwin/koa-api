fa06341cce243329a87e7a7aff76d4ba24f2e24f
ttymed <tyler.boright@gmail.com>
2017-03-07 18:35:48 -0800

    can now save users in the /cookies route using generator functions wrapped in co

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const co = require('co')
 
 const app = new Koa()
 const router = new Router()
@@ -8,33 +9,72 @@ const Mongorito = require('mongorito')
 const Model = Mongorito.Model
 
 const User = Model.extend({
-  collection: () => {
-    return 'users'
-  }
+  collection: 'users'
+})
+
+Mongorito.connect('localhost/jobsUsers')
+
+const saveNewUser = co.wrap(function *(ctx) {
+    Mongorito.connect('localhost/jobsUsers')
+    const jobIds = []
+
+    for (let i = 0; i < (Math.random() * 20); i++) {
+      jobIds.push('jobId')
+    }
+
+    var record = new User({
+      username: `user${Math.random() * 100000}`,
+      password: 'secret',
+      jobIds: jobIds 
+    })
+    yield record.save()
+    return ctx.body = "saved user"
+})
+
+
+const findAllUsers = co.wrap(function* (ctx) {
+  console.log('tyler')
+  const users = yield User.all()
+  return ctx.body = 'Returned all users'
 })
 
 const findTimeInSeconds = (startTime) => (new Date() - startTime) / 1000
 
-app.on('error', (err, ctx) =>
-    log.error('server error', err, ctx)
+const setResTimeHeader = async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const sec = findTimeInSeconds(start)
+  ctx.set('X-Response-Time', `${sec} seconds`)
+  return console.log(`${ctx.method} ${ctx.originalUrl} - ${sec}`)
+}
+
+const setBaseBody = (ctx, next) => {
+  ctx.body = "This is a default, change when you start to implement the front-end"
+  return next()
+}
+
+router.get('/cookies', 
+  setResTimeHeader,
+  setBaseBody,
+  saveNewUser
 )
 
-app.use(async function (ctx, next) {
-  const start = new Date()
-  await next()
-  const ms = findTimeInSeconds(start)
-  ctx.set('X-Response-Time', `${ms} seconds`)
-})
+router.get('users/create',
+   setResTimeHeader,
+   function (ctx, next) {
+     console.log('creating user')
+     return next()
+   },
+   saveNewUser
+)
 
-app.use(async function (ctx, next) {
-  const start = new Date()
-  await next()
-  const ms = findTimeInSeconds(start)
-  console.log(`${ctx.method} ${ctx.originalUrl} - ${ms}`)
-})
+router.get('users',
+   setResTimeHeader,
+   findAllUsers
+)
 
-app.use(ctx => {
-  ctx.body = "Is this the real life?"
-})
+app
+  .use(router.routes())
+
 
 app.listen(3000)
