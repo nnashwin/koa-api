01d55599c0f470b3f954b76db5a10c3fc8a88387
ttymed <tyler.boright@gmail.com>
2017-03-07 16:55:20 -0800

    changes the npm start to not use --harmony; adds the koa router package and imports it into the app.js

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const Koa = require('koa')
+const Router = require('koa-router')
+
 const app = new Koa()
+const router = new Router()
+
+const Mongorito = require('mongorito')
+const Model = Mongorito.Model
+
+const User = Model.extend({
+  collection: () => {
+    return 'users'
+  }
+})
 
 const findTimeInSeconds = (startTime) => (new Date() - startTime) / 1000
+
+app.on('error', (err, ctx) =>
+    log.error('server error', err, ctx)
+)
 
 app.use(async function (ctx, next) {
   const start = new Date()
