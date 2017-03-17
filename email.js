272936155d40a8eb85f803f528ca5f3889be084d
ttymed <tyler.boright@gmail.com>
2017-03-16 20:46:02 -0700

    adds code to send email to email address through node

--- email.js
+++ email.js
@@ -1,2 +1,16 @@
+const email = require("emailjs")
 const emailCred = require('./email-creds.json')
-console.log(emailCred)
+
+const server = email.server.connect({
+  user: emailCred.user,
+  password: emailCred.password,
+  host: emailCred.host,
+  ssl: emailCred.ssl
+})
+
+server.send({
+  text: "test-email",
+  from: "sunny <fake.email@gmail.com>",
+  to: "rulai <fake.email.2@gmail.com",
+  subject: "testing emailjs"
+}, function (err, msg) {console.log(err || msg)})
