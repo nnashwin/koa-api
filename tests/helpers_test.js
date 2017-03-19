b107392516fa61f876c86d6fbf2641be76410e4f
ttymed <tyler.boright@gmail.com>
2017-03-18 23:12:30 -0700

    adds the bcrypt module, babel requirements, and a failing test for the convertToHash helpers function

--- tests/helpers_test.js
+++ tests/helpers_test.js
@@ -1,4 +1,9 @@
+require('babel-register')
+require('babel-polyfill')
+const bcrypt = require('bcrypt')
+
 import { findTimeInSeconds } from '../helpers.js'
+import { convertToHash,  convertToPass } from '../helpers.js'
 import test from 'ava'
 
 test('findTimeInSeconds returns a number', t => {
@@ -6,4 +11,16 @@ test('findTimeInSeconds returns a number', t => {
   let time = findTimeInSeconds(start)
 
   t.is(typeof time, 'number')
+})
+
+test('convertToHash converts a plaintextPass to a bcrypt accurate hash', async t => {
+  const saltRounds = 10
+  const pass = "cookies"
+  let actualHash = await convertToHash(pass, saltRounds)
+  bcrypt.compare(pass, actualHash, (err, res) => {
+    console.log(res)
+    if (res === false) {
+      t.fail("the convertToHash function doesn't convert a password to accurate hash")
+    }
+  })
 })
