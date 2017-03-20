e65edf0576860cfd357229f7ad1b6525953e76c6
ttymed <tyler.boright@gmail.com>
2017-03-20 12:35:11 +0800

    adds the koa-bodyparser to the app in app.use

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const BodyParser = require('koa-bodyparser')
 const app = new Koa()
 
 const router = require('./users').router
 
 app
+  .use(BodyParser())
   .use(router.routes())
 
 
