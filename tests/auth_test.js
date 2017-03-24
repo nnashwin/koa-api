5c5b7ac97e64d2e1649c4307a2b15279afbe0394
ttymed <tyler.boright@gmail.com>
2017-03-24 09:54:17 +0800

    fixes auth_test to use a try catch to test the signJWTPromise await function and adds failures if there is an error or if the data is wrong

--- tests/auth_test.js
+++ tests/auth_test.js
@@ -1,12 +1,21 @@
 const jwt = require('jsonwebtoken')
-import { signJWTPromise } from '../auth.js'
+import { signJWTPromise, verifyJWTPromise } from '../auth.js'
 import test from 'ava'
-console.log(signJWTPromise)
 
 const secret = 'cookies'
 
 test('test that signJWTPromise returns a JWT token', async t => {
-  let actualToken = await signJWTPromise({exp: Math.floor(Date.now() / 1000) + (5 * 60), data: 'milk'}, secret)
+  try {
+    var actualToken = await signJWTPromise({exp: Math.floor(Date.now() / 1000) + (5 * 60), data: 'milk'}, secret)
+  } catch (e) {
+    t.fail(e)
+  }
   let decoded = jwt.verify(actualToken, secret)
-  console.log(decoded)
+  if (decoded.data !== 'milk') {
+    t.fail("the jwt wasn't copied correctly")
+  }
+})
+
+test('test that verifyJWTPromise returns the value from a JWT token', async t => {
+
 })
