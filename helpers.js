b3c3a072a4b0362f09e2324cb787ed3cfc9b06c3
ttymed <tyler.boright@gmail.com>
2017-03-26 23:00:14 +0800

    moves code from the users file to the helpers file in order to use in multiple routes

--- helpers.js
+++ helpers.js
@@ -1,7 +1,8 @@
+import { verifyJWTPromise } from './auth'
+import secret from './secret'
+
 const bcrypt = require('bcrypt')
-
 const findTimeInSeconds = (startTime) => (new Date() - startTime) / 1000
-
 const convertToHashPromise = (plaintextPass, saltRounds = 10) => {
   return new Promise((resolve, reject) => {
     bcrypt.hash(plaintextPass, saltRounds, (err, hash) => {
@@ -19,13 +20,45 @@ const setResTimeHeader = async (ctx, next) => {
   const sec = findTimeInSeconds(start)
   ctx.set('X-Response-Time', `${sec} seconds`)
   console.log(`${ctx.method} ${ctx.originalUrl} - ${sec}`)
-  ctx.response.status = 200
+  if (ctx.response.status === 404) {
+    ctx.response.status = 200
+  }
 
   return ctx.response
+}
+
+const rejectNonAppJsonReq = async (ctx, next) => {
+  if (ctx.is('application/json') === false)  {
+    return ctx.response.status = 400
+  }
+  await next()
+}
+
+const buildRejReq = (...fields) => async (ctx, next) => {
+  for (let field of fields) {
+    if (ctx.request.body[field] === undefined) {
+      return ctx.response.status = 400
+    }
+  }
+
+  await next()
+}
+
+const verifyJWT = async (ctx, next) => {
+  try {
+    var jwt = await verifyJWTPromise(ctx.request.body.jwt, secret.secret)
+  } catch (e) {
+    ctx.response.status = 401
+    ctx.response.errorMsg = e
+  }
+  await next()
 }
 
 module.exports = {
   findTimeInSeconds: findTimeInSeconds,
   convertToHashPromise: convertToHashPromise,
-  setResTimeHeader: setResTimeHeader
+  setResTimeHeader: setResTimeHeader,
+  rejectNonAppJsonReq,
+  buildRejReq,
+  verifyJWT
 }
