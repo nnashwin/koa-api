8a2895c2ad0a361c31a79fd5515d6b2ab367a968
ttymed <tyler.boright@gmail.com>
2017-03-27 22:14:35 +0800

    adds test to test if the checkPassWithHashPromise returns true when the password is the same

--- tests/helpers_test.js
+++ tests/helpers_test.js
@@ -2,7 +2,7 @@ require('babel-register')
 require('babel-polyfill')
 const bcrypt = require('bcrypt')
 
-import { findTimeInSeconds, convertToHashPromise,  convertToPass } from '../helpers.js'
+import { findTimeInSeconds, convertToHashPromise,  checkPassWithHashPromise } from '../helpers.js'
 import test from 'ava'
 
 test('findTimeInSeconds returns a number', t => {
@@ -17,13 +17,25 @@ test('convertToHash converts a plaintextPass to a bcrypt accurate hash', async t
   const pass = "cookies"
   let actualHash = await convertToHashPromise(pass, saltRounds)
   bcrypt.compare(pass, actualHash, (err, res) => {
-    console.log(actualHash)
     if (err) {
-      console.log(err)
       t.fail(err)
     }
     if (res === false) {
       t.fail("the convertToHash function doesn't convert a password to accurate hash")
     }
   })
+})
+
+test('checkPassWithHashPromise returns true when the password is real', async t => {
+  const saltRounds = 10
+  const pass = "cookies"
+  var hash = bcrypt.hashSync(pass, saltRounds)
+  try {
+    var isPasswordSame = checkPassWithHashPromise(pass, hash)
+  } catch (e) {
+    t.fail(e)
+  }
+  if (isPasswordSame === false) {
+    t.fail('the checkPassWithHashPromise did not return true when the pass was the same')
+  }
 })
