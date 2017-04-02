e5482ca1b584cee381429aeec88033de995a5ac4
ttymed <tyler.boright@gmail.com>
2017-04-02 18:54:59 +0800

    cleans up the app.js file some by adding semicolons and adding a PORT var and console.log to let devs know which port the service is running on

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const Koa = require('koa')
-const BodyParser = require('koa-bodyparser')
-const app = new Koa()
+import { users } from './routes/users';
+import { jobs } from './routes/jobs';
 
-import { users } from './routes/users'
-import { jobs } from './routes/jobs'
+const Koa = require('koa');
+const BodyParser = require('koa-bodyparser');
+const app = new Koa();
+const PORT = 9001;
+
 
 app
   .use(BodyParser())
   .use(users.routes())
-  .use(jobs.routes())
+  .use(jobs.routes());
 
+console.log(`Koa App running on port: ${PORT}`)
 
-app.listen(9001)
+app.listen(PORT);
