380d729a344159bf7b44ee3c76e16a7515b9a4f6
ttymed <tyler.boright@gmail.com>
2017-04-04 01:18:50 +0800

    adds eslint and properties and the appropriate packages to the package.json; changes the auth.js file to match the lint

--- auth.js
+++ auth.js
@@ -1,28 +1,29 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const signJWTPromise = (obj, secret) => {
-  return new Promise((resolve, reject) => {
-    jwt.sign(obj, secret, { algorithm: 'HS256' }, (err, token) => {
-      if (err) {
-        reject(err)
-      }
-      resolve(token)
-    })
-  })
-}
+	return new Promise((resolve, reject) => {
+		jwt.sign(obj, secret, { algorithm: 'HS256' }, (err, token) => {
+			if (err) {
+				reject(err);
+			}
+
+			resolve(token);
+		});
+	});
+};
 
 const verifyJWTPromise = (token, secret) => {
- return new Promise((resolve, reject) => {
-   jwt.verify(token, secret, (err, decoded) => {
-     if (err) {
-       reject(err)
-     }
-     resolve(decoded)
-   })
- })
-}
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, secret, (err, decoded) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(decoded);
+		});
+	});
+};
 
 module.exports = {
-  signJWTPromise: signJWTPromise,
-  verifyJWTPromise: verifyJWTPromise
-}
+	signJWTPromise,
+	verifyJWTPromise,
+};
